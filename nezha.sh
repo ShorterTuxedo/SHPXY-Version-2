51a4beef62f0480d079561b9b9181f3425bf547c
John Doe <49613009+ShorterTuxedo@users.noreply.github.com>
2025-03-22 21:56:55 +0800

    Update nezha.sh

--- nezha.sh
+++ nezha.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-# 传哪吒三个参数
-NEZHA_SERVER=$1
-NEZHA_PORT=$2
-NEZHA_KEY=$3
+# 傳哪咤和敖丙三個參數
+NEZHA_AOBING_SERVER=$1
+NEZHA_AOBING_PORT=$2
+NEZHA_AOBING_KEY=$3
 
-# 三个变量不全则不安装哪吒客户端
+# 三個變量不全則不安裝哪咤和敖丙客戶端
 check_variable() {
-  [[ -z "${NEZHA_SERVER}" || -z "${NEZHA_PORT}" || -z "${NEZHA_KEY}" ]] && exit 0
+  [[ -z "${NEZHA_AOBING_SERVER}" || -z "${NEZHA_AOBING_PORT}" || -z "${NEZHA_AOBING_KEY}" ]] && exit 0
 }
 
-# 安装系统依赖
+# 安裝系統依賴
 check_dependencies() {
   DEPS_CHECK=("wget" "unzip")
   DEPS_INSTALL=(" wget" " unzip")
@@ -18,16 +18,16 @@ check_dependencies() {
   [ -n "$DEPS" ] && { apt-get update >/dev/null 2>&1; apt-get install -y $DEPS >/dev/null 2>&1; }
 }
 
-# 下载最新版本 Nezha Agent
+# 下載最新版本 哪咤 Aobing Agent
 download_agent() {
   URL=$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
   wget -t 2 -T 10 -N ${URL}
   unzip -qod ./ nezha-agent_linux_amd64.zip && rm -f nezha-agent_linux_amd64.zip
 }
 
-# 运行客户端
+# 運行客戶端
 run() {
-  ./nezha-agent -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY}
+  ./nezha-agent -s ${NEZHA_AOBING_SERVER}:${NEZHA_AOBING_PORT} -p ${NEZHA_AOBING_KEY}
 }
 
 check_variable
